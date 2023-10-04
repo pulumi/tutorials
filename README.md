154eaf8321609afcebdd0816ddca486be682a7c6
diana esteves <diana@pulumi.com>
2023-10-04 11:39:43 -0500

    template updated

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
+# Tutorial: Deploy a "Hello World" Cloudflare Worker via Pulumi
+
 
 The main files are:
 
-Pulumi.yaml (the project file)
-Pulumi.dev.yaml (the stack configuration file)
-package.json (Node.js package information for dependencies)
-tsconfig.json (TypeScript compiler options)
-index.ts (main program file)
-
+- Pulumi.yaml (the project file)
+- Pulumi.dev.yaml (the stack configuration file)
+- package.json (Node.js package information for dependencies)
+- tsconfig.json (TypeScript compiler options)
+- index.ts (main program file)
--- index.ts
+++ index.ts
@@ -4,8 +4,39 @@ import * as cloudflare from "@pulumi/cloudflare";
 const config = new pulumi.Config();
 const accountId = config.require("accountId");
 const zoneId = config.require("zoneId");
+const domain = config.require("domain")
 
+// An Output to test the settings are set up correctly
+export const myFirstOutput = "accountId: " + accountId + ", zoneId:" + zoneId + ", domain: " + domain
 
-// Test these are set up
-export const testAccountId = accountId;
-export const testZoneId = zoneId;
+// A Worker scrpit to invoke
+export const script = new cloudflare.WorkerScript("hello-world-script", {
+    accountId: accountId,
+    name: "hello-world",
+    content: `
+        addEventListener("fetch", event => {
+          event.respondWith(handleRequest(event.request))
+        });
+
+        async function handleRequest(request) {
+          return new Response('Hello from my Worker!', { status: 200 });
+        }
+        `
+});
+
+// A Worker route to serve requests and the Worker script
+export const route = new cloudflare.WorkerRoute("hello-world-route", {
+    zoneId: zoneId,
+    pattern: "hello-world." + domain,
+    scriptName: script.name,
+
+});
+
+// A DNS record to access the route from the domain
+export const record = new cloudflare.Record("hello-world-record", {
+    zoneId: zoneId,
+    name: script.name,
+    value: "192.0.2.1",
+    type: "A",
+    proxied: true
+});
