20c49a879063b3fbea5b2798e411156fc19f3008
diana esteves <diana@pulumi.com>
2023-10-05 14:56:46 -0500

    updating template

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as cloudflare from "@pulumi/cloudflare";
-import * as fs from "fs";
 
 const config = new pulumi.Config();
 const accountId = config.require("accountId");
@@ -9,28 +7,3 @@ const domain = config.require("domain")
 
 // An Output to test the settings are set up correctly
 export const myFirstOutput = "accountId: " + accountId + ", zoneId:" + zoneId + ", domain: " + domain
-
-// A Worker scrpit to invoke
-export const script = new cloudflare.WorkerScript("hello-world-script", {
-  accountId: accountId,
-  name: "hello-world",
-  // Read the content of the worker from a file
-  content: fs.readFileSync("./app/index.ts", "utf8"),
-});
-
-// A Worker route to serve requests and the Worker script
-export const route = new cloudflare.WorkerRoute("hello-world-route", {
-  zoneId: zoneId,
-  pattern: "hello-world." + domain,
-  scriptName: script.name,
-
-});
-
-// A DNS record to access the route from the domain
-export const record = new cloudflare.Record("hello-world-record", {
-  zoneId: zoneId,
-  name: script.name,
-  value: "192.0.2.1",
-  type: "A",
-  proxied: true
-});
