4f0b9f3f3a0bfaa2721fbe5de15dfb08ceba72b9
Torian Crane <toriancrane@gmail.com>
2023-10-10 09:12:09 +0200

    Update cfn-deploy.sh

--- cfn-deploy.sh
+++ cfn-deploy.sh
@@ -2,12 +2,13 @@
 
 pip3 install -r requirements.txt
 
+projectId=$(( RANDOM % 90000000 + 10000000 ))
+
 aws cloudformation deploy \
 --template-file aws/infra.yaml \
---stack-name pulumi-esc-tutorial-stack \
---capabilities CAPABILITY_NAMED_IAM \
---region us-west-2 \
---tags owner=docs-team
+--stack-name pulumi-esc-tutorial-stack-$projectId \
+--capabilities CAPABILITY_IAM \
+--parameter-overrides deploymentId=$projectId\
 
 echo "Please see the below for your application output values: "
 
